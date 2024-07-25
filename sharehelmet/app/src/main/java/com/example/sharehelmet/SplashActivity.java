2b7a16fdd67b783c2b9110d6da403b3f06c6964c
ysh020702 <ysh020702@gmail.com>
2024-07-25 14:58:53 +0900

    splashActivity 추가, manifest수정

---
+++ sharehelmet/app/src/main/java/com/example/sharehelmet/SplashActivity.java
@@ -0,0 +1,31 @@
+package com.example.sharehelmet;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                //intent.putExtra("isLogout",0);
+                startActivity(intent);
+                finish();
+            }
+        }, 500);
+    }
+}
