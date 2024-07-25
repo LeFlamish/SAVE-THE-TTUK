77874af0cc802e531495a1b11470b6b6de8419c3
ysh020702 <ysh020702@gmail.com>
2024-07-26 03:44:21 +0900

    회원가입, 로그인 액티비티 (기능 구현 X, 로그인 누르면 메인으로 바로 연결됨)

--- sharehelmet/app/src/main/java/com/example/sharehelmet/LoginActivity.java
+++ sharehelmet/app/src/main/java/com/example/sharehelmet/LoginActivity.java
@@ -4,15 +4,24 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
+    FirebaseAuth mFirebaseAuth;         //파이어 베이스 인증
+    DatabaseReference mDatabaseRef;     //실시간 데이터 베이스
+    EditText editMAIL, editPW;
     Button loginBtn, signupBtn;
 
     @Override
@@ -26,23 +35,71 @@ public class LoginActivity extends AppCompatActivity {
 
 
     protected void setViews(){
+        mFirebaseAuth = FirebaseAuth.getInstance();
+        mDatabaseRef = FirebaseDatabase.getInstance().getReference();
+
+        editMAIL = findViewById(R.id.editMAIL);
+        editPW = findViewById(R.id.editPW);
         loginBtn = findViewById(R.id.loginbtn);
         signupBtn = findViewById(R.id.signupbtn);
+
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                //TODO : 로그인 기능 구현하기
+
+
+
+                String message = "일단 입력 안 하고도 메인이랑 연결되게 해둠";
+                //showCustomToast(message);
+                Toast.makeText(LoginActivity.this, message, Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(LoginActivity.this, MainActivity.class);
                 startActivity(intent);
-            }
-        });
+
+/*
+                String userMAIL = editMAIL.getText().toString().trim();
+                String userPW = editPW.getText().toString().trim();
+                if(userMAIL.isEmpty()){
+                    editMAIL.setError("이메일을 입력해주세요");
+                    editMAIL.requestFocus();
+                    return;
+                }
+                if(userPW.isEmpty()){
+                    editPW.setError("PW를 입력해주세요");
+                    editPW.requestFocus();
+                    return;
+                }
+                mFirebaseAuth.signInWithEmailAndPassword(userMAIL, userPW).addOnCompleteListener(LoginActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+                            startActivity(intent);
+                            //overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+                            finish();
+                        } else {
+                            //커스텀 토스트 메시지 제작 가능
+                            String message = "로그인에 실패했어요\n아이디나 비밀번호를 확인해주세요";
+                            //showCustomToast(message);
+                            Toast.makeText(LoginActivity.this, message, Toast.LENGTH_SHORT).show();
+                        }
+                    }//onComplete
+                });//onCompleteListener
+
+ */
+            }//onClick
+        });//onClickListener
+
         signupBtn.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
-                //TODO : 회원가입 액티비티 만들어서 연결짓기
-                Toast.makeText(LoginActivity.this, "회원가입 액티비티 작업 예정", Toast.LENGTH_SHORT).show();
-            }
-        });
+                String message = "회원가입 xml만 만들고 자바 안 짰어요";
+                //showCustomToast(message);
+                Toast.makeText(LoginActivity.this, message, Toast.LENGTH_SHORT).show();
+                
+                Intent intent = new Intent(LoginActivity.this, RegisterActivity.class);
+                startActivity(intent);
+            }//onClick
+        });//onClickListener
     }
 
 
--- sharehelmet/app/src/main/java/com/example/sharehelmet/User.java
+++ sharehelmet/app/src/main/java/com/example/sharehelmet/User.java
@@ -2,14 +2,14 @@ package com.example.sharehelmet;
 
 public class User {
     private String nickname;
-    private String ID;
+    private String Email;
     //보안 ㅈ까
     private String PW;
 
     public String getNickname() {return nickname;}
     public void setNickname(String nickname) {this.nickname = nickname;}
-    public String getID() {return ID;}
-    public void setID(String ID) {this.ID = ID;}
+    public String getID() {return Email;}
+    public void setID(String Email) {this.Email = Email;}
     public String getPW() {return PW;}
     public void setPW(String PW) {this.PW = PW;}
 }
