8ba637d71012c2bbccdf87f754e7d854613d664c
JinHyeongJun <wlsgudwns112@naver.com>
2024-07-26 07:08:56 +0900

    5번 탭 수정 및 계정 정보 수정 액티비티 추가

--- sharehelmet/app/src/main/java/com/example/sharehelmet/ProfileFrag5.java
+++ sharehelmet/app/src/main/java/com/example/sharehelmet/ProfileFrag5.java
@@ -1,14 +1,31 @@
 package com.example.sharehelmet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.RelativeLayout;
 
 public class ProfileFrag5 extends Fragment {
+
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.frag5_profile, container, false);
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.frag5_profile, container, false);
+
+        RelativeLayout profileSection = view.findViewById(R.id.profile_section);
+        profileSection.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), ProfileUpdateActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
