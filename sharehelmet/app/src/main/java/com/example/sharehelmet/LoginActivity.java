ad56ff6c3f2780dce5b83ffafd57ad757e8867d9
ysh020702 <ysh020702@gmail.com>
2024-07-25 15:56:53 +0900

    로그인 액티비티 완성

--- sharehelmet/app/src/main/java/com/example/sharehelmet/LoginActivity.java
+++ sharehelmet/app/src/main/java/com/example/sharehelmet/LoginActivity.java
@@ -1,6 +1,10 @@
 package com.example.sharehelmet;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +13,37 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class LoginActivity extends AppCompatActivity {
+    Button loginBtn, signupBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_login);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        setViews();
+    }
+
+
+
+    protected void setViews(){
+        loginBtn = findViewById(R.id.loginbtn);
+        signupBtn = findViewById(R.id.signupbtn);
+        loginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //TODO : 로그인 기능 구현하기
+                Intent intent = new Intent(LoginActivity.this, MainActivity.class);
+
+            }
+        });
+        signupBtn.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                //TODO : 회원가입 액티비티 만들어서 연결짓기
+                Toast.makeText(LoginActivity.this, "회원가입 액티비티 작업 예정", Toast.LENGTH_SHORT).show();
+            }
         });
     }
+
+
 }
