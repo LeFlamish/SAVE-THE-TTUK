8e4cb82e9f5551edbbe15e5fe00b5b4ec84bd16d
JinHyeongJun <wlsgudwns112@naver.com>
2024-07-27 00:36:50 +0900

    그래들 커밋

--- sharehelmet/app/build.gradle.kts
+++ sharehelmet/app/build.gradle.kts
@@ -1,3 +1,6 @@
+import org.gradle.kotlin.dsl.support.kotlinCompilerOptions
+
+
 plugins {
     alias(libs.plugins.android.application)
     id("com.google.gms.google-services")
@@ -30,6 +33,8 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+
 }
 
 dependencies {
@@ -55,6 +60,23 @@ dependencies {
     implementation("com.google.android.gms:play-services-location:21.3.0")
 
     //부트페이
-    implementation("io.github.bootpay:android:+") //최신 버전 추천
-    implementation("io.github.bootpay:android-bio:+") //생체인증 결제 사용시 추가
+    implementation("io.github.bootpay:android:4.4.3") //최신 버전 추천
+    implementation("io.github.bootpay:android-bio:4.4.21") //생체인증 결제 사용시 추가
+
+    //뷰페이저
+    implementation("androidx.viewpager2:viewpager2:1.1.0")
+    implementation("com.google.android.material:material:1.12.0")
+
+    //카톡 문의
+//    implementation("com.kakao.sdk:plusfriend:+")
+    implementation("com.kakao.sdk:v2-all:2.20.3") // 전체 모듈 설치, 2.11.0 버전부터 지원
+    implementation("com.kakao.sdk:v2-user:2.20.3") // 카카오 로그인 API 모듈
+    implementation("com.kakao.sdk:v2-share:2.20.3") // 카카오톡 공유 API 모듈
+    implementation("com.kakao.sdk:v2-talk:2.20.3") // 카카오톡 채널, 카카오톡 소셜, 카카오톡 메시지 API 모듈
+    implementation("com.kakao.sdk:v2-friend:2.20.3") // 피커 API 모듈
+    implementation("com.kakao.sdk:v2-navi:2.20.3") // 카카오내비 API 모듈
+    implementation("com.kakao.sdk:v2-cert:2.20.3") // 카카오톡 인증 서비스 API 모듈
+
+    //카톡 서비스 위한 JVM 설정 관련.
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.22")
 }
