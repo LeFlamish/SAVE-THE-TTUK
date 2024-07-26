ee33710cbf587d31460655adb45a1ddf7704dfb6
JinHyeongJun <wlsgudwns112@naver.com>
2024-07-26 14:54:10 +0900

    라이딩 가이드 틀 완료

---
+++ sharehelmet/app/src/main/java/com/example/sharehelmet/RidingGuideActivity.java
@@ -0,0 +1,39 @@
+package com.example.sharehelmet;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.viewpager2.widget.ViewPager2;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+public class RidingGuideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_riding_guide);
+
+        ViewPager2 viewPager = findViewById(R.id.viewPager_onBoarding);
+        TabLayout tabLayout = findViewById(R.id.tab_onBoarding);
+        Button nextButton = findViewById(R.id.button_next);
+
+        ViewPagerAdapter adapter = new ViewPagerAdapter(this);
+        viewPager.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager, (tab, position) -> {
+            // Customize tab indicators here if needed
+        }).attach();
+
+        nextButton.setOnClickListener(v -> {
+            int currentItem = viewPager.getCurrentItem();
+            if (currentItem < adapter.getItemCount() - 1) {
+                viewPager.setCurrentItem(currentItem + 1);
+            } else {
+                // 마지막 페이지
+                finish();
+            }
+        });
+    }
+}
---
+++ sharehelmet/app/src/main/java/com/example/sharehelmet/ViewPagerAdapter.java
@@ -0,0 +1,33 @@
+package com.example.sharehelmet;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentActivity;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+
+public class ViewPagerAdapter extends FragmentStateAdapter {
+
+    public ViewPagerAdapter(@NonNull FragmentActivity fragmentActivity) {
+        super(fragmentActivity);
+    }
+
+    @NonNull
+    @Override
+    public Fragment createFragment(int position) {
+        switch (position) {
+            case 0:
+                return new GuideFrag1();
+            case 1:
+                return new GuideFrag2();
+            case 2:
+                return new GuideFrag3();
+            default:
+                return new GuideFrag1();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return 3;
+    }
+}
