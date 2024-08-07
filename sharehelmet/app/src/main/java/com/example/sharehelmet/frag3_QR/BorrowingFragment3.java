c911df47d112e28868b933bd72a0930fb94b5673
SeungHo <seungho4136@naver.com>
2024-08-07 17:34:27 +0900

    Revert "대여 화면에 지도 추가"
    This reverts commit 21613c69107da373b2144175a8c90e62c921413a.

--- sharehelmet/app/src/main/java/com/example/sharehelmet/frag3_QR/BorrowingFragment3.java
+++ sharehelmet/app/src/main/java/com/example/sharehelmet/frag3_QR/BorrowingFragment3.java
@@ -1,16 +1,8 @@
 package com.example.sharehelmet.frag3_QR;
 
-import static android.Manifest.permission.ACCESS_FINE_LOCATION;
-
 import android.annotation.SuppressLint;
-import android.content.pm.PackageManager;
-import android.content.res.Configuration;
-import android.graphics.Color;
-import android.location.Location;
 import android.os.Bundle;
 import androidx.annotation.NonNull;
-import androidx.core.app.ActivityCompat;
-import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import android.os.Handler;
 import android.os.Looper;
@@ -19,10 +11,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import com.example.sharehelmet.R;
-import com.example.sharehelmet.frag1_home.HomeFrag1;
 import com.example.sharehelmet.model.Helmet;
 import com.example.sharehelmet.model.User;
 import com.google.firebase.database.DataSnapshot;
@@ -30,26 +19,12 @@ import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-import com.naver.maps.geometry.LatLng;
-import com.naver.maps.map.LocationTrackingMode;
-import com.naver.maps.map.MapView;
-import com.naver.maps.map.NaverMap;
-import com.naver.maps.map.OnMapReadyCallback;
-import com.naver.maps.map.overlay.Marker;
-import com.naver.maps.map.util.FusedLocationSource;
-
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Locale;
 
-public class BorrowingFragment3 extends Fragment implements OnMapReadyCallback {
-    private static final int LOCATION_PERMISSION_REQUEST_CODE = 1000;
-    private MapView mapView;
-    private NaverMap naverMap;
-    private FusedLocationSource locationSource;
+public class BorrowingFragment3 extends Fragment {
     private String firebaseId;
     private String helmetId;
     private User user;
@@ -61,23 +36,10 @@ public class BorrowingFragment3 extends Fragment implements OnMapReadyCallback {
     private DatabaseReference db;
     private LocalDateTime rentalStartTime;
     private Button returnButton;
-    private List<Marker> markerList = new ArrayList<>();
-    private List<Place> places = new ArrayList<>();
-    private List<Place> allPlaces = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_borrowing3, container, false);
-        try{
-            mapView = view.findViewById(R.id.map);
-            mapView.onCreate(savedInstanceState);
-            mapView.getMapAsync(this);
-            locationSource = new FusedLocationSource(this, LOCATION_PERMISSION_REQUEST_CODE);
-        }catch(Exception ignored){}
-        Button btnModeLocation = view.findViewById(R.id.btn_current_location);
-        btnModeLocation.setOnClickListener(v -> {
-            naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
-        });
         db = FirebaseDatabase.getInstance().getReference();
         t11 = view.findViewById(R.id.helmetId);
         t12 = view.findViewById(R.id.battery);
@@ -161,96 +123,6 @@ public class BorrowingFragment3 extends Fragment implements OnMapReadyCallback {
             long seconds = duration.getSeconds() % 60;
             String elapsedTime = String.format(Locale.getDefault(), "%02d:%02d:%02d", hours, minutes, seconds);
             t14.setText(elapsedTime);
-        }
-    }
-
-    @Override
-    public void onMapReady(@NonNull NaverMap naverMap) {
-        try{
-            this.naverMap = naverMap;
-            naverMap.setLocationSource(locationSource);
-            if (ContextCompat.checkSelfPermission(getContext(), ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                ActivityCompat.requestPermissions(getActivity(), new String[]{ACCESS_FINE_LOCATION}, LOCATION_PERMISSION_REQUEST_CODE);
-            } else {
-                naverMap.setLocationTrackingMode(LocationTrackingMode.Follow);
-            }
-            naverMap.addOnLocationChangeListener(location -> {
-                displayNearbyPlaces(location);
-            });
-            int nightModeFlags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
-            if (nightModeFlags == Configuration.UI_MODE_NIGHT_YES) {
-                naverMap.setMapType(NaverMap.MapType.Navi); // 다크 모드
-                naverMap.setNightModeEnabled(true);
-            } else {
-                naverMap.setMapType(NaverMap.MapType.Basic); // 라이트 모드
-                naverMap.setNightModeEnabled(false);
-            }
-        }catch(Exception ignored){}
-    }
-    private void displayNearbyPlaces(Location currentLocation) {
-        try{
-            if (currentLocation == null) {
-                return;
-            }
-            LatLng currentLatLng = new LatLng(currentLocation.getLatitude(), currentLocation.getLongitude());
-            db.child("places").addListenerForSingleValueEvent(new ValueEventListener() {
-                @Override
-                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                    clearMarkers();
-                    allPlaces.clear();
-                    for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
-                        String id = snapshot.getKey();
-                        double latitude = snapshot.child("latitude").getValue(Double.class);
-                        double longitude = snapshot.child("longitude").getValue(Double.class);
-                        int stock = snapshot.child("stock").getValue(Integer.class);
-                        String name = snapshot.getKey();
-                        LatLng placeLatLng = new LatLng(latitude, longitude);
-                        double distance = calculateDistance(currentLatLng, placeLatLng);
-                        Place place = new Place(id, placeLatLng, distance, stock, name);
-                        allPlaces.add(place);
-                        if (distance <= 1.5&&stock!=20) {
-                            Marker marker = new Marker();
-                            marker.setPosition(placeLatLng);
-                            marker.setCaptionText(name);
-                            marker.setMap(naverMap);
-                            markerList.add(marker);
-                        }
-                    }
-                }
-                @Override
-                public void onCancelled(@NonNull DatabaseError databaseError) {
-                    Toast.makeText(getContext(), "데이터를 불러오는데 실패했습니다.", Toast.LENGTH_SHORT).show();
-                }
-            });
-        }catch (Exception ignored){
-
-        }
-    }
-    private double calculateDistance(LatLng start, LatLng end) {
-        float[] results = new float[1];
-        Location.distanceBetween(start.latitude, start.longitude, end.latitude, end.longitude, results);
-        return results[0] / 1000.0; // 미터를 킬로미터로 변환
-    }
-
-    private void clearMarkers() {
-        for (Marker marker : markerList) {
-            marker.setMap(null);
-        }
-        markerList.clear();
-    }
-    public static class Place {
-        LatLng latLng;
-        double distance;
-        int stock;
-        String name;
-        String id;
-
-        Place(String id, LatLng latLng, double distance, int stock, String name) {
-            this.id = id;
-            this.latLng = latLng;
-            this.distance = distance;
-            this.stock = stock;
-            this.name = name;
         }
     }
 }
