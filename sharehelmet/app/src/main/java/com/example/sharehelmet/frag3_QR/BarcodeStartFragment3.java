bf0552c5f09c8377e1a884761f5360403a00718f
SeungHo <seungho4136@naver.com>
2024-08-08 02:18:57 +0900

    대여시 거리 계산

--- sharehelmet/app/src/main/java/com/example/sharehelmet/frag3_QR/BarcodeStartFragment3.java
+++ sharehelmet/app/src/main/java/com/example/sharehelmet/frag3_QR/BarcodeStartFragment3.java
@@ -7,6 +7,10 @@ import android.content.DialogInterface;
 import android.content.pm.PackageManager;
 import android.hardware.camera2.CameraAccessException;
 import android.hardware.camera2.CameraManager;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -22,8 +26,12 @@ import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 import com.example.sharehelmet.R;
+import com.example.sharehelmet.home.MainActivity;
 import com.example.sharehelmet.model.Storage;
 import com.example.sharehelmet.model.User;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -34,6 +42,8 @@ import com.journeyapps.barcodescanner.BarcodeCallback;
 import com.journeyapps.barcodescanner.BarcodeResult;
 import com.journeyapps.barcodescanner.DecoratedBarcodeView;
 import com.journeyapps.barcodescanner.DefaultDecoderFactory;
+import com.naver.maps.geometry.LatLng;
+
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
@@ -53,12 +63,15 @@ public class BarcodeStartFragment3 extends Fragment {
     private String insteadQrResult;
     private ImageButton turnOnLight, writeCode;
     private static final Pattern CODE_PATTERN = Pattern.compile("^\\d{3}-\\d{3}$");
+    private static final int LOCATION_REQUEST_CODE = 1000;
+    private FusedLocationProviderClient fusedLocationClient;
 
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_barcode_start3, container, false);
         db = FirebaseDatabase.getInstance().getReference();
         barcodeView = view.findViewById(R.id.barcode_scanner);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
         Bundle bundle = getArguments();
         if (bundle != null) {
             firebaseId=bundle.getString("firebaseId");
@@ -67,6 +80,9 @@ public class BarcodeStartFragment3 extends Fragment {
             ActivityCompat.requestPermissions(requireActivity(), new String[]{Manifest.permission.CAMERA}, CAMERA_PERMISSION_REQUEST_CODE);
         } else {
             barcodeView.resume();
+        }
+        if (ContextCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(getActivity(), new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, LOCATION_REQUEST_CODE);
         }
         barcodeView.getBarcodeView().setDecoderFactory(new DefaultDecoderFactory(Collections.singletonList(BarcodeFormat.QR_CODE)));
         barcodeView.decodeContinuous(new BarcodeCallback() {
@@ -96,7 +112,18 @@ public class BarcodeStartFragment3 extends Fragment {
                     user = snapshot.getValue(User.class);
                     if (user != null) {
                         barcodeView.pause();
-                        BorrowHelmet1();
+                        fusedLocationClient.getLastLocation().addOnSuccessListener(getActivity(), new OnSuccessListener<Location>() {
+                            @Override
+                            public void onSuccess(Location location) {
+                                if (location != null) {
+                                    double myLatitude = location.getLatitude();
+                                    double myLongitude = location.getLongitude();
+                                    BorrowHelmet1(myLatitude, myLongitude);
+                                } else {
+                                    Toast.makeText(getContext(), "현재 위치를 가져올 수 없습니다.", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        });
                         barcodeView.resume();
                     }
                 }
@@ -107,7 +134,18 @@ public class BarcodeStartFragment3 extends Fragment {
             Toast.makeText(getContext(), "유효하지 않은 QR코드입니다", Toast.LENGTH_SHORT).show();
         }
     }
-    private void BorrowHelmet1() {
+    public double haversine(double lat1, double lon1, double lat2, double lon2) {
+        final int R = 6371; // Radius of the Earth in kilometers
+        double latDistance = Math.toRadians(lat2 - lat1);
+        double lonDistance = Math.toRadians(lon2 - lon1);
+        double a = Math.sin(latDistance / 2) * Math.sin(latDistance / 2)
+                + Math.cos(Math.toRadians(lat1)) * Math.cos(Math.toRadians(lat2))
+                * Math.sin(lonDistance / 2) * Math.sin(lonDistance / 2);
+        double c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+        double distance = R * c;
+        return distance*1000;
+    }
+    private void BorrowHelmet1(double myLatitude, double myLongitude) {
         String locationID = storageId.substring(0, 3);
         int helmetIndex = Integer.parseInt(storageId.substring(4, 7)) - 1;
         db.child("places").orderByChild("locationID").equalTo(locationID).addListenerForSingleValueEvent(new ValueEventListener() {
@@ -118,52 +156,64 @@ public class BarcodeStartFragment3 extends Fragment {
                         String placeKey = placeSnapshot.getKey();
                         Storage storage = placeSnapshot.getValue(Storage.class);
                         if (storage != null) {
-                            ArrayList<String> storedHelmetID = storage.getStoredHelmetID();
-                            if (storedHelmetID != null && helmetIndex >= 0 && helmetIndex < storedHelmetID.size()) {
-                                String helmetId = storedHelmetID.get(helmetIndex);
-                                if (!"-".equals(helmetId)) {
+                            double storageLatitude = storage.getLatitude();
+                            double storageLongitude = storage.getLongitude();
+                            double length_between_me_storage = haversine(myLatitude, myLongitude, storageLatitude, storageLongitude);
+                            if(length_between_me_storage<=10000){
+                                ArrayList<String> storedHelmetID = storage.getStoredHelmetID();
+                                if (storedHelmetID != null && helmetIndex >= 0 && helmetIndex < storedHelmetID.size()) {
+                                    String helmetId = storedHelmetID.get(helmetIndex);
+                                    if (!"-".equals(helmetId)) {
 
-                                    //토스트 메세지
-                                    Toast.makeText(getContext(), "No."+helmetId+" 헬멧 대여", Toast.LENGTH_SHORT).show();
+                                        //토스트 메세지
+                                        Toast.makeText(getContext(), "No."+helmetId+" 헬멧 대여", Toast.LENGTH_SHORT).show();
 
-                                    //places 파베 수정
-                                    storedHelmetID.set(helmetIndex, "-");
-                                    storage.setStoredHelmetID(storedHelmetID);
-                                    storage.setStock(storage.getStock()-1);
-                                    db.child("places").child(placeKey).setValue(storage);
+                                        //places 파베 수정
+                                        storedHelmetID.set(helmetIndex, "-");
+                                        storage.setStoredHelmetID(storedHelmetID);
+                                        storage.setStock(storage.getStock()-1);
+                                        db.child("places").child(placeKey).setValue(storage);
 
-                                    //Users 파베 수정
-                                    LocalDateTime rentalStartTime = LocalDateTime.now(); // 대여 시작 시간 저장
-                                    DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
-                                    String formattedStartTime = rentalStartTime.format(formatter);
-                                    ArrayList<String> rental_info=new ArrayList<>();
-                                    rental_info.add(helmetId);
-                                    rental_info.add(formattedStartTime);
-                                    user.setNow_qr(1);
-                                    user.setRental_info(rental_info);
-                                    db.child("users").child(firebaseId).setValue(user);
+                                        //Users 파베 수정
+                                        LocalDateTime rentalStartTime = LocalDateTime.now(); // 대여 시작 시간 저장
+                                        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss", Locale.getDefault());
+                                        String formattedStartTime = rentalStartTime.format(formatter);
+                                        ArrayList<String> rental_info=new ArrayList<>();
+                                        rental_info.add(helmetId);
+                                        rental_info.add(formattedStartTime);
+                                        user.setNow_qr(1);
+                                        user.setRental_info(rental_info);
+                                        db.child("users").child(firebaseId).setValue(user);
 
-                                    //프래그먼트 이동
-                                    BorrowingFragment3 borrowingFragment3=new BorrowingFragment3();
-                                    Bundle bundle = new Bundle();
-                                    bundle.putString("firebaseId",firebaseId);
-                                    borrowingFragment3.setArguments(bundle);
-                                    getActivity().getSupportFragmentManager().beginTransaction()
-                                            .replace(R.id.fragment_container, borrowingFragment3)
-                                            .commit();
-                                }
-                                else {
+                                        //프래그먼트 이동
+                                        BorrowingFragment3 borrowingFragment3=new BorrowingFragment3();
+                                        Bundle bundle = new Bundle();
+                                        bundle.putString("firebaseId",firebaseId);
+                                        borrowingFragment3.setArguments(bundle);
+                                        getActivity().getSupportFragmentManager().beginTransaction()
+                                                .replace(R.id.fragment_container, borrowingFragment3)
+                                                .commit();
+                                    }
+                                    else {
+                                        Toast.makeText(getContext(), "보관함이 비어있습니다.", Toast.LENGTH_SHORT).show();
+                                    }
+                                } else {
                                     Toast.makeText(getContext(), "보관함이 비어있습니다.", Toast.LENGTH_SHORT).show();
                                 }
-                            } else {
-                                Toast.makeText(getContext(), "보관함이 비어있습니다.", Toast.LENGTH_SHORT).show();
                             }
-                        } else {
+                            else{
+                                Toast.makeText(getContext(), "보관함이 너무 멀리 있습니다.", Toast.LENGTH_SHORT).show();
+                            }
+
+
+                        }
+                        else {
                             Toast.makeText(getContext(), "보관함 데이터를 찾지 못했습니다.", Toast.LENGTH_SHORT).show();
                         }
                         return;
                     }
-                } else {
+                }
+                else {
                     Toast.makeText(getContext(), "보관함이 존재하지 않습니다", Toast.LENGTH_SHORT).show();
                 }
             }
@@ -173,7 +223,6 @@ public class BarcodeStartFragment3 extends Fragment {
             }
         });
     }
-
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
